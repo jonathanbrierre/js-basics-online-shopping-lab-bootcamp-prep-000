67545044e34051ca5c2cd2b9b364ef6fabbcf2d9
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-05 20:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,8 +58,6 @@ function removeFromCart(item) {
     while (i < cart.length){
       if (cart[i]['itemName'] === item){
          cart.splice(i,1);
-        // var newCart = cart;
-        // return newCart;
       }
       else if (i < cart.length){
         i++

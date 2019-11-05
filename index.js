5338fb66f4dbe7a899c6267f9b25c522600d7890
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-05 18:40:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  
  var newItem = { 
    itemName: item,
    itemPrice: Math.floor(Math.random() * 100) + 1 
-   
- }
+ };
+ cart.push(newItem);
+ return `${item} has been added to your cart.`
+ 
 }
 
 function viewCart() {

f98d0ca1393e212ab5f2ef022afafd9b84618f16
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-05 18:35:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   { itemName: item,
-    itemPrice: Math.floor(Math.random()*100) }
+    itemPrice: Math.floor(Math.random() * 100) + 1 }
 }
 
 function viewCart() {
